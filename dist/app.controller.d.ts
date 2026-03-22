c039c1d3416fc1e6046a411a017ed665918434b6
aliwrwr <aliwrwr@github.com>
2026-03-22 03:19:44 +0200

    cleanup: remove temp clear-groups endpoint

--- dist/app.controller.d.ts
+++ dist/app.controller.d.ts
@@ -1,16 +1,7 @@
 import { AppService } from './app.service';
-import { GroupsService } from './groups/groups.service';
 export declare class AppController {
     private readonly appService;
-    private readonly groupsService;
-    constructor(appService: AppService, groupsService: GroupsService);
-    clearGroupsTemp(secret: string): Promise<{
-        error: string;
-        cleared?: undefined;
-    } | {
-        cleared: boolean;
-        error?: undefined;
-    }>;
+    constructor(appService: AppService);
     getHello(): string;
     getSystemStats(): Promise<{
         cpu: number;
